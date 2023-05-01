470a35660901582132af58344331bfe4c614c5a9
akadir8 <bickiabdulkadir12@gmail.com>
2023-05-01 19:50:57 +0300

    modified

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ import Logo from "./unnamed.png"
 
 function Navbar() {
   return (
-    <nav className="flex items-center justify-between flex-wrap p-5 bg-gradient-to-r from-indigo-500 from-30% to-sky-500">
+    <nav className="flex items-center justify-between flex-wrap p-5 bg-gradient-to-r from-emerald-400 from-30% to-sky-500">
       <div className="flex items-center flex-shrink-0 text-white mr-6">
         <img className="ml-8" src={Logo} height={40} width={40}/>
         <span className="font-semibold text-xl tracking-tight ml-8">Abdulkadir Biçki</span>
@@ -36,7 +36,7 @@ function Navbar() {
           </a>
         </div>
         <div>
-          <button className="inline-block bg-gradient-to-r from-sky-500 from-30% to-indigo-500 text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-blue-500 hover:bg-white mt-4 lg:mt-0 hover:shadow-md">Benimle çalışmak ister misin?</button>
+          <button className="inline-block bg-gradient-to-r from-sky-500 from-30% to-emerald-400 text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-blue-500 hover:bg-white mt-4 lg:mt-0 hover:shadow-md">Benimle çalışmak ister misin?</button>
         </div>
       </div>
     </nav>
