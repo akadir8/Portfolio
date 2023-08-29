c7d020775185392bff6786cbb3c4145e97f44e8e
akadir8 <bickiabdulkadir12@gmail.com>
2023-08-29 18:53:50 +0300

    updated

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
 import React from "react";
-//components
-import Banner from "./components/Banner";
-import Header from "./components/Header";
-import Navbar from "./components/Navbar";
-import About from "./components/About";
-import Contact from "./components/Contact";
-import Footer from "./components/Footer";
-
+import { Header } from "./components/Header";
 function App() {
   return (
-    <div className="bg-no-repeat bg-cover overflow-hidden">
-      <Header/>
-      <Banner/>
-      <Navbar/>
-      <About/>
-      <Contact/>
-
-     
-      <Footer />
+    <div >
+  
+     <Header />
+      
     </div>
   );
 }
