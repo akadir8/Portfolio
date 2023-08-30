0a6a57c9f9b6ad7862b21075566b05ba8053cf6f
akadir8 <bickiabdulkadir12@gmail.com>
2023-08-30 15:00:30 +0300

    updated

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+function Home() {
+    return <section>
+        <div className="h-screen flex flex-col items-center justify-center text-center space-y-8 overflow-hidden">
+            <div className="relative flex justify-center items-center" style={{opacity:1, borderRadius:20%, transform:"none"}}>
+
+            </div>
+        </div>
+    </section>
+}
+export default Home;
