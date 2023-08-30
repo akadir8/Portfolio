71075f9e5a98e5d854e64d254b96bdb98aa21645
akadir8 <bickiabdulkadir12@gmail.com>
2023-08-30 15:00:24 +0300

    updated

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { Header } from "./components/Header";
+import Home from "./components/Home";
 function App() {
   return (
     <div >
   
      <Header />
-      
+      <Home />
     </div>
   );
 }
