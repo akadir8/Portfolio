03a3a0fdee8f92a28ca533828d1876d328710c67
akadir8 <bickiabdulkadir12@gmail.com>
2023-09-04 18:41:49 +0300

    updated

--- src/components/Home.js
+++ src/components/Home.js
@@ -65,6 +65,7 @@ function Home() {
             <a href="/#skills">
               <button className="heroButton text-[#626974] hover:text-black dark:text-gray-100 dark:hover:text-gray-500">Skills</button>
             </a>
+            
           </div>
         </div>
       </div>
