c4cd575f8488d774c4539566e376fa87d1bcb15a
akadir8 <bickiabdulkadir12@gmail.com>
2023-09-13 19:28:59 +0300

    updated

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React from "react";
 import { Header } from "./components/Header";
 import Home from "./components/Home";
 import About from "./components/About";
+import ParticlesContainer from "./components/ParticlesContainer";
 function App() {
   return (
-    <div >
-  
-     <Header />
+    <div>
+      <ParticlesContainer />
+      <Header />
       <Home />
       <About />
     </div>
---
+++ src/components/ParticlesContainer.js
@@ -0,0 +1,93 @@
+import { Particles } from "react-particles-js";
+import { loadFull } from "react-tsparticles";
+import React, { useCallback } from "react";
+
+const ParticlesContainer = () => {
+  // init
+  const particlesInit = useCallback(async engine => {
+    await loadFull(engine);
+    // you can initialize the tsParticles instance (main)
+  }, []);
+
+  const particlesLoaded = useCallback(async () => {}, []);
+
+  return (
+    <Particles
+      className="w-full h-full absolute translate-z-0"
+      id="tsparticles"
+      init={particlesInit}
+      loaded={particlesLoaded}
+      options={{
+        fullScreen: { enable: false },
+        background: {
+          color: {
+            value: "",
+          },
+        },
+        fpsLimit: 120,
+        interactivity: {
+          events: {
+            onclick: {
+              enable: false,
+              mode: "push",
+            },
+            onHover: { enable: true, mode: "repulse" },
+            resize: true,
+          },
+          modes: {
+            push: {
+              quantity: 90,
+            },
+            repulse: {
+              distance: 200,
+              duration: 0.4,
+            },
+          },
+        },
+        particles: {
+          color: {
+            value: "#e68e2e",
+          },
+          links: {
+            color: "#f5d393",
+            distance: 150,
+            enable: true,
+            opacity: 0.5,
+            width: 1,
+          },
+          collisions: {
+            enable: true,
+          },
+          move: {
+            directions: "none",
+            enable: true,
+            outModes: {
+              default: "bounce",
+            },
+            random: false,
+            speed: 1,
+            staright: false,
+          },
+          number: {
+            density: {
+              enable: true,
+              area: 800,
+            },
+            value: 80,
+          },
+          opacity: {
+            value: 0.5,
+          },
+          shape: {
+            type: "circle",
+          },
+          size: {
+            value: { min: 1, max: 5 },
+          },
+        },
+        detectRetina: true,
+      }}
+    />
+  );
+};
+export default ParticlesContainer;
